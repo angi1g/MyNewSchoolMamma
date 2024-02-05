1b1e0d10319de8ed99f0687b4c794c1f2719d6ba
Giacomo Angione <angi1g@gmail.com>
2024-02-05 12:24:22 +0100

    Initial Commit

---
+++ MyNewSchoolMamma/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyNewSchoolMamma
+//
+//  Created by Barbara on 05/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyNewSchoolMamma/MyNewSchoolMammaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNewSchoolMammaApp.swift
+//  MyNewSchoolMamma
+//
+//  Created by Barbara on 05/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MyNewSchoolMammaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
